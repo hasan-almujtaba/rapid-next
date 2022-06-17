4ff55d93fe883e729add040794a08d5863a0b3e7
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-06-17 17:17:59 +0700

    add ui component library (mantine)

---
+++ components/layouts/default.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  useMantineColorScheme,
+  Button,
+} from '@mantine/core'
+import { DefaultLayout } from '../../types/default-layout.interface'
+import { BsSunFill, BsMoonFill } from 'react-icons/bs'
+
+const DefaultLayout = ({ children }: DefaultLayout) => {
+  /**
+   * Use mantine theme and color scheme
+   */
+  const { colors } = useMantineTheme()
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+
+  /**
+   * Toggle Navbar visibility
+   */
+  const [opened, setOpened] = useState(false)
+
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background: colorScheme === 'dark' ? colors.dark[8] : colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="xl"
+      fixed
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="xl"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      footer={
+        <Footer
+          height={60}
+          p="md"
+        >
+          Application footer
+        </Footer>
+      }
+      header={
+        <Header
+          height={70}
+          p="md"
+        >
+          <div
+            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+          >
+            <MediaQuery
+              largerThan="md"
+              styles={{ display: 'none' }}
+            >
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Application header</Text>
+
+            <div style={{ flexGrow: 1 }}></div>
+
+            <Button
+              variant="subtle"
+              color={colorScheme === 'dark' ? 'yellow' : 'dark'}
+            >
+              {colorScheme === 'dark' ? (
+                <BsSunFill
+                  size="24"
+                  onClick={() => toggleColorScheme()}
+                />
+              ) : (
+                <BsMoonFill
+                  size="24"
+                  onClick={() => toggleColorScheme()}
+                />
+              )}
+            </Button>
+          </div>
+        </Header>
+      }
+    >
+      {children}
+    </AppShell>
+  )
+}
+
+export default DefaultLayout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,57 @@
-import '../styles/globals.css'
+import { GetServerSidePropsContext } from 'next'
 import { AppPropsWithLayout } from '../types/layout.type'
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core'
+import { useState } from 'react'
+import { getCookie, setCookies } from 'cookies-next'
 
-const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+const App = ({
+  Component,
+  pageProps,
+  preferredColorScheme,
+}: AppPropsWithLayout & { preferredColorScheme: ColorScheme }) => {
   /**
    * Use the layout defined at the page level, if available
    */
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  /**
+   * Detect user's preferred color scheme
+   */
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme)
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
+    setColorScheme(nextColorScheme)
+    setCookies('mantine-color-scheme', nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    })
+  }
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        theme={{ colorScheme }}
+        withNormalizeCSS
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
+
+/**
+ * Get the user's preferred color scheme from the cookie
+ */
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  preferredColorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+})
+
 export default App
