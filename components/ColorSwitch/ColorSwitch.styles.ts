c4a97dea4f2e267e9c314d11abab5f6d315cfcfc
Hasan Almujtaba <hasan.almujtaba01@gmail.com>
2022-07-29 08:25:58 +0700

    add new color mode component

---
+++ components/ColorSwitch/ColorSwitch.styles.ts
@@ -0,0 +1,29 @@
+import { createStyles } from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+    '& *': {
+      cursor: 'pointer',
+    },
+  },
+
+  icon: {
+    pointerEvents: 'none',
+    position: 'absolute',
+    zIndex: 1,
+    top: 3,
+  },
+
+  iconLight: {
+    left: 4,
+    color: theme.white,
+  },
+
+  iconDark: {
+    right: 4,
+    color: theme.colors.gray[6],
+  },
+}))
+
+export default useStyles
