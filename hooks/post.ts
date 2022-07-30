432f3e04a51898528500e5962e3387e07d8c1251
Hasan Almujtaba <hasan.almujtaba01@gmail.com>
2022-07-31 03:52:14 +0700

    remove example file

--- hooks/post.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query'
-import { fetchPosts } from '@/apis/post'
-
-/**
- * Reusable react query custom hook
- */
-export const usePosts = () => {
-  return useQuery('posts', () => fetchPosts())
-}
