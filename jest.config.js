735a5f2cbff88481e0c93446e106d37ca38d6218
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-11-16 13:35:56 +0700

    feat: :sparkles: add jest
    add jest and rtl for unit testing

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
