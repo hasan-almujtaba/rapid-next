488599d2e4440f18813583a0e89f9d793ff4d2f3
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2023-01-30 19:49:17 +0700

    chore: update .linstagedrc.js configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,21 +1,4 @@
-const path = require('path')
-
-const buildTscCommand = (filenames) =>
-  `tsc-files --noEmit ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
-
-const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`
-
 module.exports = {
-  '*.{ts,tsx}': [buildTscCommand],
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', () => 'tsc'],
+  '*.{css}': 'prettier --write',
 }
