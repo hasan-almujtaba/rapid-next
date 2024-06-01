048eb158bfacfaeefaab19a447dc41be85fc36d4
Hasan Al Mujtaba <hasan.almujtaba01@gmail.com>
2024-06-02 04:19:16 +0700

    remove unused login form attribute

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -19,7 +19,7 @@ export const LoginForm = () => {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
@@ -34,8 +34,6 @@ export const LoginForm = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
             className="space-y-6"
-            action="#"
-            method="POST"
             onSubmit={handleSubmit(onSubmit)}
           >
             <div>
